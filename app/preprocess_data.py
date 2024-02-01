e430b60c2d65a38a1e55e93331a0d52282abdaf8
David LaJambe <dlajambe@gmail.com>
2024-01-31 21:12:49 -0500

    Add spacy tokenizer

--- app/preprocess_data.py
+++ app/preprocess_data.py
@@ -3,16 +3,26 @@ import pandas as pd
 
 from modules.logger import initialize_logger
 from config import Config
+from spacy.lang.en import English
 
 def preprocess_data():
     initialize_logger('preprocess_data', Config.log_root)
     logging.info('Beginning preprocess_data.py')
     logging.info('Loading data')
+    tokenizer = English().tokenizer
 
     chunk_idx = 1
-    for chunk in pd.read_csv(Config.data_path_raw, chunksize=Config.chunk_size):
-        print(chunk_idx)
+    for chunk in pd.read_csv(Config.data_path_raw, chunksize=1):
+
         chunk_idx += 1
+        chunk_en = chunk['en'].iloc[0]
+        doc = tokenizer(chunk_en)
+        tokens = [token.text for token in doc]
+        print('Raw: {}\tTokenized: {}'.format(chunk_en, tokens))
+
+        if chunk_idx > 10:
+            print(type(tokens))
+            break    
     
     logging.info('Preprocessing data')
 
